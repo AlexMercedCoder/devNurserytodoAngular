50774752ed745c7d78bdcf8835ccce85529217b5
Alex Merced - AlexMercedCoder.com <alexmerced@alexmerced.com>
2019-12-26 11:56:21 -0500

    to do list should be good to go

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'devnurtodo';
+  userInfo;
+  loggedIN = false;
+  storedTodos;
+  todos = ['example todo'];
+
+  constructor(private http: HttpClient) { }
+
+  addTodo (form) {
+      console.log(form.form)
+      this.todos.push(form.form.controls.todo.value);
+      const user = this.userInfo.user
+      const body = {
+          entry: form.form.controls.todo.value,
+          user: user
+      }
+      fetch(`https://buffaloapidevn.herokuapp.com/todoes/`, {
+          method: 'post',
+          body: JSON.stringify(body)
+      }).then (res => console.log(res))
+      window.localStorage.setItem('devNtodos',JSON.stringify(this.todos));
+  }
+
+  removeTodo (index, id) {
+      this.todos.splice(index,1)
+      const user = this.userInfo.user
+      fetch(`https://buffaloapidevn.herokuapp.com/todoes/${id}`, {
+          method: 'delete',
+      }).then (res => console.log(res))
+      window.localStorage.setItem('devNtodos',JSON.stringify(this.todos));
+  }
+
+  ngOnInit (){
+      this.userInfo = JSON.parse(window.localStorage.getItem('devNursery'))
+      this.storedTodos = JSON.parse(window.localStorage.getItem('devNtodos'))
+      if (this.userInfo){
+          this.loggedIN = true;
+          if (this.storedTodos){
+              this.todos = this.storedTodos;
+          } else {
+              fetch(`https://buffaloapidevn.herokuapp.com/todos/${this.userInfo.user}`)
+              .then((res) => res.json())
+              .then((json) => this.todos = json)
+          }
+      }
+
+  }
 }
